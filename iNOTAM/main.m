a253f78b2cfcbdf0b95ee49a8250e6e692be9af9
Eiichi HAYASHI <cdp53190@gmail.com>
2014-12-24 17:14:29 +0900

    Initial Commit

---
+++ iNOTAM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iNOTAM
+//
+//  Created by 林 英市 on 2014/12/24.
+//  Copyright (c) 2014年 skyElements. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
